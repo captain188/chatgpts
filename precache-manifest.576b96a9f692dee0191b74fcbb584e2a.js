0adae0a855db6b79f9845abaa43c8d8d36a3b693
captain188 <78192031+captain188@users.noreply.github.com>
2024-08-09 10:01:17 +0800

    0809 backup

---
+++ precache-manifest.576b96a9f692dee0191b74fcbb584e2a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fa5b7471597b4dc3853",
+    "url": "./d032240921e3b8f45d06.module.wasm"
+  },
+  {
+    "revision": "7146bb11a6ccd59745d36249363164bc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9c0b34d5b5800d389830",
+    "url": "./static/css/2.c412fb8a.chunk.css"
+  },
+  {
+    "revision": "8a8535b72a6bbb341c19",
+    "url": "./static/css/main.34bf5f7b.chunk.css"
+  },
+  {
+    "revision": "9c0b34d5b5800d389830",
+    "url": "./static/js/2.621c87c6.chunk.js"
+  },
+  {
+    "revision": "5fa5b7471597b4dc3853",
+    "url": "./static/js/3.753cf703.chunk.js"
+  },
+  {
+    "revision": "8a8535b72a6bbb341c19",
+    "url": "./static/js/main.b66d995c.chunk.js"
+  },
+  {
+    "revision": "061a04ec949471f55ade",
+    "url": "./static/js/runtime-main.6a483d65.js"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "./static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "./static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "./static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "./static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "./static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "./static/media/icons-20.cef8cdbb.woff"
+  }
+]);
